5b6cc3e0103f9b31c185fda97324d8f5ba6a4851
LazarMilosavljevic <milosavljeviclazar56@gmail.com>
2025-07-27 17:48:02 +0200

    Dodate su metode u BibliotekaInterface

--- src/biblioteka/interfejs/BibliotekaInterface.java
+++ src/biblioteka/interfejs/BibliotekaInterface.java
@@ -1,5 +1,17 @@
 package biblioteka.interfejs;
 
+import java.util.List;
+
+import biblioteka.Knjiga;
+
 public interface BibliotekaInterface {
 
+	
+	public void dodajKnjigu(Knjiga knjiga) ;
+	
+	public void obrisiKnjigu(Knjiga knjiga);
+	
+	public List<Knjiga> vratiListuKnjiga();
+	
+	public List<Knjiga> pronadjiKnjigu(Knjiga knjiga);
 }
